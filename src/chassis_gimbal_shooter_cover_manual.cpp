78ad9ccc562c1cc8001a8a478f7354585066d471
liyixin135 <421159734@qq.com>
2024-02-28 00:17:05 +0800

    Comment.

--- src/chassis_gimbal_shooter_cover_manual.cpp
+++ src/chassis_gimbal_shooter_cover_manual.cpp
@@ -27,7 +27,7 @@ ChassisGimbalShooterCoverManual::ChassisGimbalShooterCoverManual(ros::NodeHandle
   z_event_.setFalling(boost::bind(&ChassisGimbalShooterCoverManual::zRelease, this));
 }
 
-void ChassisGimbalShooterCoverManual::changeSpeedMode(SpeedMode speed_mode)
+void ChassisGimbalShooterCoverManual::changeSpeedMode(SpeedMode speed_mode)  // 给予速度模式
 {
   if (speed_mode == LOW)
   {
@@ -39,14 +39,14 @@ void ChassisGimbalShooterCoverManual::changeSpeedMode(SpeedMode speed_mode)
   }
 }
 
-void ChassisGimbalShooterCoverManual::updatePc(const rm_msgs::DbusData::ConstPtr& dbus_data)
+void ChassisGimbalShooterCoverManual::updatePc(const rm_msgs::DbusData::ConstPtr& dbus_data)  // 更新dbus信息
 {
   ChassisGimbalShooterManual::updatePc(dbus_data);
   gimbal_cmd_sender_->setRate(-dbus_data->m_x * gimbal_scale_,
                               cover_command_sender_->getState() ? 0.0 : dbus_data->m_y * gimbal_scale_);
 }
 
-void ChassisGimbalShooterCoverManual::checkReferee()
+void ChassisGimbalShooterCoverManual::checkReferee()  // 检查裁判系统
 {
   manual_to_referee_pub_data_.cover_state = cover_command_sender_->getState();
   if (switch_detection_srv_->getTarget() != rm_msgs::StatusChangeRequest::ARMOR)
@@ -55,14 +55,14 @@ void ChassisGimbalShooterCoverManual::checkReferee()
     manual_to_referee_pub_data_.det_target = switch_detection_srv_->getTarget();
   ChassisGimbalShooterManual::checkReferee();
 }
-void ChassisGimbalShooterCoverManual::checkKeyboard(const rm_msgs::DbusData::ConstPtr& dbus_data)
+void ChassisGimbalShooterCoverManual::checkKeyboard(const rm_msgs::DbusData::ConstPtr& dbus_data)  // 检查键盘
 {
   ChassisGimbalShooterManual::checkKeyboard(dbus_data);
   ctrl_z_event_.update(dbus_data->key_ctrl & dbus_data->key_z);
   z_event_.update((!dbus_data->key_ctrl) & dbus_data->key_z);
 }
 
-void ChassisGimbalShooterCoverManual::sendCommand(const ros::Time& time)
+void ChassisGimbalShooterCoverManual::sendCommand(const ros::Time& time)  // 发送命令
 {
   if (supply_)
   {
@@ -130,27 +130,27 @@ void ChassisGimbalShooterCoverManual::rightSwitchUpRise()
   supply_ = false;
 }
 
-void ChassisGimbalShooterCoverManual::rPress()
+void ChassisGimbalShooterCoverManual::rPress()  // 按下r
 {
-  if (switch_buff_srv_->getTarget() != rm_msgs::StatusChangeRequest::ARMOR)
+  if (switch_buff_srv_->getTarget() != rm_msgs::StatusChangeRequest::ARMOR)  // 如果目标不是装甲板
   {
-    chassis_cmd_sender_->power_limit_->updateState(rm_common::PowerLimit::CHARGE);
-    if (switch_buff_type_srv_->getTarget() == rm_msgs::StatusChangeRequest::SMALL_BUFF)
-      switch_buff_type_srv_->setTargetType(rm_msgs::StatusChangeRequest::BIG_BUFF);
+    chassis_cmd_sender_->power_limit_->updateState(rm_common::PowerLimit::CHARGE);       // 底盘功率是charge
+    if (switch_buff_type_srv_->getTarget() == rm_msgs::StatusChangeRequest::SMALL_BUFF)  // 如果目标是小幅
+      switch_buff_type_srv_->setTargetType(rm_msgs::StatusChangeRequest::BIG_BUFF);      // 更改为打大幅
     else
-      switch_buff_type_srv_->setTargetType(rm_msgs::StatusChangeRequest::SMALL_BUFF);
-    switch_buff_type_srv_->callService();
+      switch_buff_type_srv_->setTargetType(rm_msgs::StatusChangeRequest::SMALL_BUFF);  // 大幅变为小幅
+    switch_buff_type_srv_->callService();                                              // call服务
   }
   else
-    chassis_cmd_sender_->power_limit_->updateState(rm_common::PowerLimit::NORMAL);
+    chassis_cmd_sender_->power_limit_->updateState(rm_common::PowerLimit::NORMAL);  // 底盘功率是normal
 }
 
-void ChassisGimbalShooterCoverManual::ePress()
+void ChassisGimbalShooterCoverManual::ePress()  // 按下e
 {
-  switch_buff_srv_->switchTargetType();
-  switch_detection_srv_->switchTargetType();
-  switch_buff_type_srv_->setTargetType(switch_buff_srv_->getTarget());
-  switch_buff_srv_->callService();
+  switch_buff_srv_->switchTargetType();                                 // 更换target为相反，0改为1,1改为0
+  switch_detection_srv_->switchTargetType();                            // 更换target为相反，0改为1,1改为0
+  switch_buff_type_srv_->setTargetType(switch_buff_srv_->getTarget());  // return target
+  switch_buff_srv_->callService();                                      // call服务
   switch_detection_srv_->callService();
   switch_buff_type_srv_->callService();
 }
